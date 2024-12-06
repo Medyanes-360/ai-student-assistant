0af5f691edd9d674c44f7bae22eb7c3d7d9deac3
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-12-06 13:48:44 +0300

    languageDevelopment - mobile adjustments and loading for chat

--- app/(chatLayout)/chat/page.jsx
+++ app/(chatLayout)/chat/page.jsx
@@ -2,7 +2,7 @@ import ChatContainer from "@/containers/chat";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col w-full h-screen  items-center py-10 bg-[url('/images/chat-bg.svg')] bg-no-repeat bg-cover">
+    <div className="flex flex-col w-full h-screen  items-center py-10 bg-[url('/images/chat-bg.svg')] bg-no-repeat bg-cover overflow-y-auto">
       <ChatContainer />
     </div>
   );
--- components/chat/languageDevelopment/index.jsx
+++ components/chat/languageDevelopment/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
@@ -77,8 +77,15 @@ const LanguageDevelopment = () => {
 
   return (
     <>
-      <div className="flex  flex-col-reverse gap-3 lg:flex-row pt-[80px] w-full">
+      <div className="flex flex-col-reverse gap-3 lg:flex-row pt-[80px] w-full px-4 md:px-0 ">
         <div className="bg-[#F4F4F4] h-full dark:bg-gray-800 rounded-lg shadow-lg p-8 w-full flex-grow-0 flex-shrink-0 basis-[70%] transition duration-200">
+          {!userText || !aiText ? (
+            <div className="text-gray-800 dark:text-gray-200 w-full justify-center animate-pulse flex">
+              Loading...
+            </div>
+          ) : (
+            ""
+          )}
           {userText && (
             <div className="mt-8 flex flex-col gap-y-2">
               <h2 className="text-xl sm:text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-4">
@@ -104,68 +111,65 @@ const LanguageDevelopment = () => {
           )}
         </div>
 
-        <div className="flex-1">
-          <div className="flex items-center flex-col gap-4">
-            <Image
-              src="/images/person.png"
-              alt="person"
-              width={1920}
-              height={1080}
-            />
-            <PushToTalk onRecordingComplete={handleRecordingComplete} />
-          </div>
+        <div className="flex items-center flex-col gap-4">
+          <Image
+            src="/images/person.png"
+            alt="person"
+            width={1920}
+            height={1080}
+          />
+          <PushToTalk onRecordingComplete={handleRecordingComplete} />
         </div>
-        <Tooltip
-          id="tooltip"
-          place="top"
-          effect="solid"
-          anchorSelect="#clickable"
-          clickable
-          openOnClick
-          className="text-lg bg-white text-gray-800 p-4 rounded-lg shadow-lg z-10 absolute"
-          style={{
-            backgroundColor: "#ffffff",
-            color: "#000000",
-            borderRadius: "8px",
-            boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
-            zIndex: 9999999,
-            padding: "0px",
-          }}
-          render={({ content, activeAnchor }) => {
-            // String contenti dönüştürüyoruz
-            const data = JSON.parse(content);
-            return (
-              <div className="p-4 max-w-md flex flex-col gap-1">
-                <h3 className="text-2xl font-bold py-2 border-b border-black/50">
-                  {data?.word}
-                </h3>
-                <div className="flex flex-col ">
-                  <p className="m-0">
-                    <strong>Pronunciation:</strong> {data?.pronunciation}
-                  </p>
-                  <p className="m-0">
-                    <strong>Translations:</strong>{" "}
-                    {data?.translations.join(", ")}
-                  </p>
-                  <p>
-                    <strong>Examples:</strong>
-                  </p>
-                  <ul className="pl-5 m-0 list-disc">
-                    {data?.examples.map((example, index) => (
-                      <li key={index}>{example}</li>
-                    ))}
-                  </ul>
-                  <div className="flex justify-between mt-4">
-                    <Button className="flex w-full justify-center gap-2 items-center !opacity-100 bg-green-500 text-white border-none py-1 px-2 rounded-lg">
-                      Learned <FaCheck />
-                    </Button>
-                  </div>
+      </div>
+      <Tooltip
+        id="tooltip"
+        place="top"
+        effect="solid"
+        anchorSelect="#clickable"
+        clickable
+        openOnClick
+        className="text-lg bg-white text-gray-800 p-4 rounded-lg shadow-lg z-10 absolute"
+        style={{
+          backgroundColor: "#ffffff",
+          color: "#000000",
+          borderRadius: "8px",
+          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
+          zIndex: 9999999,
+          padding: "0px",
+        }}
+        render={({ content, activeAnchor }) => {
+          // String contenti dönüştürüyoruz
+          const data = JSON.parse(content);
+          return (
+            <div className="p-4 max-w-md flex flex-col gap-1">
+              <h3 className="text-2xl font-bold py-2 border-b border-black/50">
+                {data?.word}
+              </h3>
+              <div className="flex flex-col ">
+                <p className="m-0">
+                  <strong>Pronunciation:</strong> {data?.pronunciation}
+                </p>
+                <p className="m-0">
+                  <strong>Translations:</strong> {data?.translations.join(", ")}
+                </p>
+                <p>
+                  <strong>Examples:</strong>
+                </p>
+                <ul className="pl-5 m-0 list-disc">
+                  {data?.examples.map((example, index) => (
+                    <li key={index}>{example}</li>
+                  ))}
+                </ul>
+                <div className="flex justify-between mt-4">
+                  <Button className="flex w-full justify-center gap-2 items-center !opacity-100 bg-green-500 text-white border-none py-1 px-2 rounded-lg">
+                    Learned <FaCheck />
+                  </Button>
                 </div>
               </div>
-            );
-          }}
-        />
-      </div>
+            </div>
+          );
+        }}
+      />
     </>
   );
 };
